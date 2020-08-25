6e133e474e029932ef4fe288d224c358b288b590
Chuazj <zijian@hotmail.sg>
2020-08-26 00:21:58 +0800

    Update server.js

--- app/server.js
+++ app/server.js
@@ -11,7 +11,7 @@ app.use(morgan('combined'));
 
 // Configuration
 var port = process.env.PORT || 8080;
-var message = process.env.MESSAGE || "Updating this part GOGOGO!!!!!!";
+var message = process.env.MESSAGE || "Hello from KARBON!";
 
 app.get('/', function (req, res) {
     res.render('home', {
